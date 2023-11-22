794bb2fa1496c2247d9fdc081799eaa2203a52cc
resetmerlin <resetmerlin@gmail.com>
2023-11-22 11:38:53 +0900

    feat: Add ItemInfoEvents organisms component

--- niksnkrs/components/organisms/index.ts
+++ niksnkrs/components/organisms/index.ts
@@ -2,3 +2,5 @@ export * from './header';
 export * from './cardLists';
 export * from './intro';
 export * from './itemLists';
+export * from './itemNav';
+export * from './itemInfoEvents';
---
+++ niksnkrs/components/organisms/itemInfoEvents/ItemInfoEvents.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { AtomicButton, AtomicSubtitle, SvgLeftArrowAlt } from '../../atoms';
+import { ItemInfo } from '../../molecules';
+import './ItemInfoEvents.scss';
+import { IProduct } from '@/lib/types';
+
+type IProps = {
+  goPrevPage: () => void;
+  addToCart: (event: React.FormEvent<HTMLFormElement>) => void;
+  product: IProduct | null;
+};
+
+/**
+ * Responsible for making item info events organisms
+ *
+ * - Responsible for components begin to have the final shape
+ * - Responsible for combination of molecules that work together or atoms that compose more elaborate interfaces
+ */
+export default function ItemInfoEvents({
+  product,
+  goPrevPage,
+  addToCart,
+}: IProps) {
+  return (
+    <form className='itemInfoEvents' onSubmit={addToCart}>
+      <AtomicButton
+        type='button'
+        shape='none'
+        className='arrow-button'
+        onClick={goPrevPage}
+      >
+        <SvgLeftArrowAlt size='8rem' color='black' />
+      </AtomicButton>
+      <ItemInfo product={product} />
+
+      <div className='itemInfoEvents__btn-wrap'>
+        <AtomicButton shape='rect' type='submit' size='xxl'>
+          <AtomicSubtitle color='tertiary' size='xl' strength='600'>
+            Order Now
+          </AtomicSubtitle>
+        </AtomicButton>
+
+        <AtomicButton shape='rect' type='submit' color='secondary' size='xxl'>
+          <AtomicSubtitle size='l' strength='600'>
+            Favorite
+          </AtomicSubtitle>
+        </AtomicButton>
+      </div>
+    </form>
+  );
+}
