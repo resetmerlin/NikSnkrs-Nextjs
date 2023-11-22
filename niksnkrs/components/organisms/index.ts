dc570a644101c9217b1a6e3b969de0c1f81fd736
resetmerlin <resetmerlin@gmail.com>
2023-11-22 09:33:21 +0900

    feat: Add ItemLists organisms component and scss file

--- niksnkrs/components/organisms/index.ts
+++ niksnkrs/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export * from './header';
 export * from './cardLists';
 export * from './intro';
+export * from './itemLists';
---
+++ niksnkrs/components/organisms/itemLists/ItemLists.tsx
@@ -0,0 +1,28 @@
+import { IProduct, IProducts } from '@/lib/types';
+import { Item } from '@/components';
+import './ItemLists.scss';
+import Link from 'next/link';
+
+type IProps = {
+  products: IProducts | undefined;
+};
+
+/**
+ * Responsible for taking a list of 'products' and rendering them as 'Item's
+ *
+ * - Responsible for components begin to have the final shape
+ * - Responsible for combination of molecules that work together or atoms that compose more elaborate interfaces
+ */
+export default function ItemLists({ products }: IProps) {
+  return (
+    <div className='item-lists'>
+      {products?.map((product: IProduct) => {
+        return (
+          <Link href={`/product/${product._id}`} key={product._id}>
+            <Item product={product} />
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
