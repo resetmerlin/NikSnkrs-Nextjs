b3044da843c365c3dfca2d2178c43df3f7210a62
resetmerlin <resetmerlin@gmail.com>
2023-11-24 08:30:15 +0900

    refactor: Add script for preventing override error

--- niksnkrs/lib/models/userModel.ts
+++ niksnkrs/lib/models/userModel.ts
@@ -43,6 +43,6 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
