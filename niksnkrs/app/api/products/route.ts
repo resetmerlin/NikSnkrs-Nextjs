c869569459f792c2eb4f78ecefdf01d008efb510
resetmerlin <resetmerlin@gmail.com>
2023-11-25 13:00:50 +0900

    feat: Add Get products api

---
+++ niksnkrs/app/api/products/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getProducts } from '@/lib/actions/products.actions';
+
+export const GET = async (req: NextRequest) => {
+  try {
+    if (req.method == 'GET') {
+      const products = await getProducts();
+
+      if (products && products.length > 0) {
+        return NextResponse.json(products);
+      } else {
+        throw new Error('Products not found');
+      }
+    } else {
+      throw new Error('Invalid request method');
+    }
+  } catch (err) {
+    return NextResponse.json({ message: err });
+  }
+};
