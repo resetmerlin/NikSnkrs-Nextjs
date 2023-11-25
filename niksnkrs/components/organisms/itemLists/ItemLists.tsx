06bdf83b414448b107e006dbbea55db46061b2de
resetmerlin <resetmerlin@gmail.com>
2023-11-25 13:46:17 +0900

    feat: Add SkeletonItem component

--- niksnkrs/components/organisms/itemLists/ItemLists.tsx
+++ niksnkrs/components/organisms/itemLists/ItemLists.tsx
@@ -1,10 +1,11 @@
 import { IProduct, IProducts } from '@/lib/types';
-import { Item } from '@/components';
+import { Item, SkeletonItem } from '@/components';
 import './ItemLists.scss';
 import Link from 'next/link';
 
 type IProps = {
   products: IProducts | undefined;
+  isLoading: boolean;
 };
 
 /**
@@ -13,16 +14,18 @@ type IProps = {
  * - Responsible for components begin to have the final shape
  * - Responsible for combination of molecules that work together or atoms that compose more elaborate interfaces
  */
-export default function ItemLists({ products }: IProps) {
+export default function ItemLists({ products, isLoading }: IProps) {
   return (
     <div className='item-lists'>
-      {products?.map((product: IProduct) => {
-        return (
-          <Link href={`/product/${product._id}`} key={product._id}>
-            <Item product={product} />
-          </Link>
-        );
-      })}
+      {isLoading
+        ? [...Array(6).keys()].map((i) => <SkeletonItem key={i} />)
+        : products?.map((product: IProduct) => {
+            return (
+              <Link href={`/product/${product._id}`} key={product._id}>
+                <Item product={product} />
+              </Link>
+            );
+          })}
     </div>
   );
 }
