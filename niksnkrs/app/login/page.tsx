62b717c86d7a507ed65ba5466149f67d367d5195
resetmerlin <resetmerlin@gmail.com>
2023-11-25 13:03:16 +0900

    feat: Add authenticate user action function

--- niksnkrs/app/login/page.tsx
+++ niksnkrs/app/login/page.tsx
@@ -8,18 +8,31 @@ import {
   ParentTemplate,
   UserForm,
 } from '@/components';
-import { loginSchema, requiredLogin } from '@/lib/schema';
+import { requiredLogin } from '@/lib/schema';
 import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { authenticationUser } from '@/lib/actions/user.actions';
+import { selectUser, userInfoAdded } from '../store/features';
+import { useDispatch, useSelector } from 'react-redux';
 
 export type LoginData = {
   userEmail: string;
   userPassword: string;
 };
 export default function Page() {
-  const loginSubmit: SubmitHandler<LoginData> = (data: LoginData) => {
-    console.log({ email: data.userEmail, password: data.userPassword });
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const loginSubmit: SubmitHandler<LoginData> = async (data: LoginData) => {
+    const loginData = await authenticationUser({
+      email: data.userEmail,
+      password: data.userPassword,
+    });
+
+    console.log(loginData);
+
+    // if (loginData) dispatch(userInfoAdded(loginData));
   };
 
   const {
