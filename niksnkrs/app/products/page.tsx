8bfbbd7ff9d84a0bdab5efaec2e3833e192248c7
resetmerlin <resetmerlin@gmail.com>
2023-11-25 13:45:23 +0900

    feat: Pass isLoading to ItemLists for skeleton

--- niksnkrs/app/products/page.tsx
+++ niksnkrs/app/products/page.tsx
@@ -7,22 +7,13 @@ import {
   ItemLists,
   ParentTemplate,
 } from '@/components';
-import { getProducts } from '@/lib/actions/products.actions';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  productAdded,
-  selectProducts,
-  useGetProductsQuery,
-} from '../store/features';
+import React from 'react';
+import { useGetProductsQuery } from '../store/features';
 
 export default function Page() {
-  const isLoading = null;
   const userInfo = null;
 
-  const { data: products } = useGetProductsQuery();
-
-  console.log(products);
+  const { data: products, isLoading } = useGetProductsQuery();
 
   const logOutHandler = () => console.log('touched');
 
@@ -33,7 +24,7 @@ export default function Page() {
           <AtomicTitle size='xs'>Latest Products</AtomicTitle>
         </ChildTemplate>
         <ChildTemplate position='bottomCenter' size='s'>
-          <ItemLists products={products} />
+          <ItemLists products={products} isLoading={isLoading} />
         </ChildTemplate>
       </ParentTemplate>
     </HeaderLayout>
