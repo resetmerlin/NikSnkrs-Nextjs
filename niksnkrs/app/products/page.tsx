ecfa639df5064c88c3fa8d06c9c399ee1d043bfd
resetmerlin <resetmerlin@gmail.com>
2023-11-25 13:01:50 +0900

    refactor: Use RTX query to handle api

--- niksnkrs/app/products/page.tsx
+++ niksnkrs/app/products/page.tsx
@@ -10,30 +10,19 @@ import {
 import { getProducts } from '@/lib/actions/products.actions';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { productAdded, selectProducts } from '../store/features';
+import {
+  productAdded,
+  selectProducts,
+  useGetProductsQuery,
+} from '../store/features';
 
 export default function Page() {
   const isLoading = null;
   const userInfo = null;
 
-  const dispatch = useDispatch();
-  const selectedProducts = useSelector(selectProducts);
-  const products = selectedProducts[0];
+  const { data: products } = useGetProductsQuery();
 
-  useEffect(() => {
-    let ignore = false;
-    if (!products || products.length == 0) {
-      const handleProducts = async () => {
-        const data = await getProducts();
-        if (!ignore) dispatch(productAdded(data));
-      };
-
-      handleProducts();
-      return () => {
-        ignore = true;
-      };
-    }
-  }, [dispatch, products]);
+  console.log(products);
 
   const logOutHandler = () => console.log('touched');
 
