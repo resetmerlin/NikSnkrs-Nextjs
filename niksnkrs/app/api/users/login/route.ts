077a5f60364665d97f91aeb84374cdeb26a8a3f8
resetmerlin <resetmerlin@gmail.com>
2023-11-26 12:50:39 +0900

    feat: Add user authenticate POST api

---
+++ niksnkrs/app/api/users/login/route.ts
@@ -0,0 +1,31 @@
+import User from '@/lib/models/userModel';
+import { connectDatabase } from '@/lib/mongoose';
+import { createToken } from '@/lib/tokens';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: { email: string; password: string }, res) => {
+  try {
+    connectDatabase();
+
+    const { email, password } = req;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      return NextResponse.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: createToken(user._id),
+      });
+    } else {
+      throw new Error('Invalid email of password');
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Invalid email or password' },
+      { status: 400 }
+    );
+  }
+};
