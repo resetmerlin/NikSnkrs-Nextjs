560a4bf96bfeffb6bbaab0a10df2ab796dc64c8b
resetmerlin <resetmerlin@gmail.com>
2023-11-28 18:26:30 +0900

    feat: Add logout hook

--- niksnkrs/app/hooks/hooks.ts
+++ niksnkrs/app/hooks/hooks.ts
@@ -1,6 +1,12 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store/store';
+import { userInfoDeleted } from '../store/features';
 
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/** Logout; Delete userInfo state  */
+export function logOut(dispatch: AppDispatch) {
+  dispatch(userInfoDeleted());
+}
