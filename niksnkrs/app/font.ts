271cf746a28b5ebcb3e1193d4f8d6dbd77c0944e
resetmerlin <resetmerlin@gmail.com>
2023-11-28 18:50:32 +0900

    feat: Add font lists

---
+++ niksnkrs/app/font.ts
@@ -0,0 +1,4 @@
+import { Goldman, Oswald } from 'next/font/google';
+
+export const oswald = Oswald({ weight: '400', subsets: ['cyrillic'] });
+export const goldmand = Goldman({ weight: '400', subsets: ['latin'] });
