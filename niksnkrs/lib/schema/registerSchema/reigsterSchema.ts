727663f9d1247f2ba5ae8d237bb96986039bd138
resetmerlin <resetmerlin@gmail.com>
2023-11-28 19:27:46 +0900

    feat: Add register schmea

--- niksnkrs/lib/schema/registerSchema/reigsterSchema.ts
+++ niksnkrs/lib/schema/registerSchema/reigsterSchema.ts
@@ -1,14 +1,19 @@
-import * as yup from 'yup';
+import { z } from 'zod';
 
-export const registerSchema = yup.object().shape({
-  userEmail: yup
-    .string()
-    .required('Please write your email')
-    .matches(/^[^\s@]+@example\.com$/, '@example.com required'),
-  userPassword: yup.string().required('Please write your password'),
-  userName: yup.string().required('Please write your name'),
-  userConfirmPassword: yup
-    .string()
-    .required('Please write your confirm password')
-    .oneOf([yup.ref('userPassword')], `Passwords won't match`),
-});
+export const registerSchema = z
+  .object({
+    userEmail: z
+      .string()
+      .regex(/^[^\s@]+@example\.com$/, '@example.com required')
+      .min(1, 'Please write your email'),
+
+    userPassword: z.string().min(1, 'Please write your password'),
+    userConfirmPassword: z
+      .string()
+      .min(1, 'Please write your confirm password'),
+    userName: z.string().min(1, 'Please write your name'),
+  })
+  .refine((data) => data.userPassword === data.userConfirmPassword, {
+    message: 'Passwords do not match',
+    path: ['userConfirmPassword'], // This specifies which field the error is associated with
+  });
