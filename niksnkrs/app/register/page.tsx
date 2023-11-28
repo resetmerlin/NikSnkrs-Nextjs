4fd86419e492a2ea125cb417940b36b1d9860eb4
resetmerlin <resetmerlin@gmail.com>
2023-11-28 19:27:11 +0900

    feat: Add hook and layout of reigster page

--- niksnkrs/app/register/page.tsx
+++ niksnkrs/app/register/page.tsx
@@ -1,5 +1,37 @@
+'use client';
+import {
+  Background,
+  ChildTemplate,
+  Layout,
+  ParentTemplate,
+  RegisterForm,
+  UserForm,
+} from '@/components';
 import React from 'react';
+import { useRegisterPage } from './page.hook';
 
 export default function Page() {
-  return <div>page</div>;
+  const [register, handleSubmit, inputErrors, registerSubmit, registerError] =
+    useRegisterPage();
+
+  return (
+    <Layout>
+      <ParentTemplate size='full'>
+        <ChildTemplate size='full' position='leftRight'>
+          <Background />
+        </ChildTemplate>
+        <ChildTemplate size='full' position='right'>
+          <UserForm>
+            <RegisterForm
+              register={register}
+              handleSubmit={handleSubmit}
+              inputErrors={inputErrors}
+              registerSubmit={registerSubmit}
+              registerError={registerError}
+            />
+          </UserForm>
+        </ChildTemplate>
+      </ParentTemplate>
+    </Layout>
+  );
 }
