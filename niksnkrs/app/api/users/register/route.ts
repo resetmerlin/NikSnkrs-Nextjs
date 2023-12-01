4943a5cf19e38def9a8a0b27dc0b14e5c37f9070
resetmerlin <resetmerlin@gmail.com>
2023-12-01 19:07:19 +0900

    refactor: Delete uncessary code and switch into lib actions

--- niksnkrs/app/api/users/register/route.ts
+++ niksnkrs/app/api/users/register/route.ts
@@ -1,56 +1,17 @@
+import { registerUser } from '@/lib/actions/user.actions';
 import User from '@/lib/models/userModel';
 import { connectDatabase } from '@/lib/mongoose';
-import { createToken } from '@/lib/tokens';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export const POST = async (
-  req: {
-    body: { name: string; email: string; password: string };
-  },
-  res
-) => {
-  await connectDatabase();
-
-  const { name, email, password } = req.body;
+export const POST = async (req: Request) => {
   try {
-    const userExists = await User.findOne({ email });
+    if (req.method !== 'POST') throw new Error('Invalid request method');
 
-    if (userExists) {
-      return NextResponse.json(
-        { message: 'User already exists' },
-        { status: 401 }
-      );
-    }
+    const body = await req.json();
 
-    // Validation
-    if (!name || !email || !password) {
-      return NextResponse.json(
-        { message: 'Name, email, and password are required' },
-        { status: 400 }
-      );
-    }
+    const user = await registerUser(body);
 
-    const user = await User.create({
-      name,
-      email,
-      password,
-      //we will gonna crypt it
-    });
-
-    if (user) {
-      return NextResponse.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: createToken(user._id),
-      });
-    } else {
-      return NextResponse.json(
-        { message: 'Invalid user data' },
-        { status: 400 }
-      );
-    }
+    return NextResponse.json(user);
   } catch (error) {
     console.error(error); // Log the error for debugging
     return NextResponse.json({ message: `${error}` }, { status: 500 });
