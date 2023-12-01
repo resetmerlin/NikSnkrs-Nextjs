d2352b52a95ea9701f3229bd08e08f292ad03e60
resetmerlin <resetmerlin@gmail.com>
2023-12-01 19:08:48 +0900

    refactor: Add body req.json() to get the body

--- niksnkrs/app/api/users/login/route.ts
+++ niksnkrs/app/api/users/login/route.ts
@@ -3,13 +3,12 @@ import { NextResponse } from 'next/server';
 
 export const POST = async (req: Request, res: NextResponse) => {
   try {
-    if (req.method == 'POST') {
-      const auth = await authenticationUser(req.body);
+    if (req.method !== 'POST') throw new Error('Invalid request method');
 
-      return NextResponse.json(auth);
-    } else {
-      throw new Error('Invalid request method');
-    }
+    const body = await req.json();
+    const auth = authenticationUser(body);
+
+    return NextResponse.json(auth);
   } catch (err) {
     return NextResponse.json({ message: `${err}` }, { status: 500 });
   }
