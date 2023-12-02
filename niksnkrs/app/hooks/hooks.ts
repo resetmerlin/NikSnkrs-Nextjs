f2270e8d593672000643918b40d5294c2de0fd62
resetmerlin <resetmerlin@gmail.com>
2023-12-02 16:59:27 +0900

    feat: Add useGoToLogin hook

--- niksnkrs/app/hooks/hooks.ts
+++ niksnkrs/app/hooks/hooks.ts
@@ -1,6 +1,9 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store/store';
 import { userInfoDeleted } from '../store/features';
+import { IUser } from '@/lib/types';
+import { useEffect } from 'react';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
@@ -9,4 +12,12 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 /** Logout; Delete userInfo state  */
 export function logOut(dispatch: AppDispatch) {
   dispatch(userInfoDeleted());
+}
+
+export function useGoToLogin(userInfo: IUser, router: AppRouterInstance) {
+  useEffect(() => {
+    if (!userInfo?.token) {
+      router.push('/login');
+    }
+  }, [router, userInfo]);
 }
