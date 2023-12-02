86efd6f3fbaa4ce25a36e0113c9e5991e06f3a1c
resetmerlin <resetmerlin@gmail.com>
2023-12-02 12:53:06 +0900

    refactor: Change position of providers with headerlayout

--- niksnkrs/app/(with-header)/layout.tsx
+++ niksnkrs/app/(with-header)/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={oswald.className}>
-        <HeaderLayout>
-          <Providers>{children}</Providers>
-        </HeaderLayout>
+        <Providers>
+          <HeaderLayout>{children}</HeaderLayout>
+        </Providers>
       </body>
     </html>
   );
